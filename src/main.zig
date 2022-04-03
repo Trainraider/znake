1a4955cc18e114631c9df74139b540d0c84b0b70
Trainraider <trainraider7@gmail.com>
2022-04-03 13:33:16 -0400

    sub-character substrate for snake traversal

--- src/main.zig
+++ src/main.zig
@@ -23,33 +23,88 @@ const Direction = enum {
     down,
 };
 
+const GridError = error {
+    OutOfBounds,
+};
+
+const Grid = struct {
+    display: *zbox.Buffer,
+    //x,y,width,height are in terms of terminal characters.
+    //In the created grid, height is double the below height
+    x: u64,
+    y: u64,
+    width: u64,
+    height: u64,
+
+    fn init(display: *zbox.Buffer, x: u64, y: u64, width: u64, height: u64) Grid {
+        const cell: zbox.Cell = .{
+            .char = '▄',
+            .attribs = .{
+                .fg_black = true,
+                .bg_black = true,
+            },
+        };
+        var i: u64 = 0;
+        while (i < width) {
+            var j: u64 = 0;
+            while (j < height) {
+                display.cellRef(j + y, i + x).* = cell;
+                j += 1;
+            }
+            i += 1;
+        }
+        return Grid{
+            .display = display,
+            .x = x,
+            .y = y,
+            .width = width,
+            .height = height,
+        };
+    }
+
+    fn setCell(self: *Grid, x: u64, y: u64, on: bool) !void {
+        if (x >= self.width or y >= self.height << 1) return GridError.OutOfBounds;
+
+        const upperHalf: bool = (y & 1 == 0 );
+        const op: u2 = (@as(u2, @boolToInt(on)) << 1) + @as(u2, @boolToInt(upperHalf));
+
+        var cell = self.*.display.cellRef(self.y + (y >> 1), self.x + x);
+        var attribs = &cell.*.attribs;
+        switch (op) {
+            0 => {cell.char = '▄'; attribs.*.fg_green = false; attribs.*.fg_black = true;},
+            1 => {cell.char = '▄'; attribs.*.bg_black = true; attribs.*.bg_green = false;},
+            2 => {cell.char = '▄'; attribs.*.fg_green = true; attribs.*.fg_black = false;},
+            3 => {cell.char = '▄'; attribs.*.bg_black = false; attribs.*.bg_green = true;},
+        }
+
+    }
+
+    fn getCell(self: Grid, x: u64, y: u64) bool {
+        const upperHalf: bool = (y & 1 == 0 );
+        const attribs = self.display.cellRef(self.y + (y >> 1), self.x + x).*.attribs;
+
+        if (upperHalf) {
+            return attribs.bg_green;
+        } else {
+            return attribs.fg_green;
+        }
+    }
+};
+
 const Snake = struct {
     x: []u64,
     y: []u64,
     dir: ?Direction = null,
     length: u64,
     alloc: Allocator,
-    display: *zbox.Buffer,
+    display: *Grid,
     dead: bool = false,
-    headCell: zbox.Cell = .{
-        .char = '⍨',
-        .attribs = .{ .bg_green = true, .fg_black = true, .bold = true,},
-    },
-    deadCell: zbox.Cell = .{
-        .char = 'ӧ',
-        .attribs = .{ .bg_green = true, .fg_black = true, .bold = true,},
-    },
-    bodyCell: zbox.Cell= .{
-        .char = ' ',
-        .attribs = .{ .bg_green = true,},
-    },
     _head: u64,
     _tail: u64,
 
-    pub fn init(alloc: Allocator, display: *zbox.Buffer, length: u64) !Snake {
+    pub fn init(alloc: Allocator, display: *Grid, length: u64) !Snake {
         assert(length > 0);
-        const size = try zbox.size();
-        const area = mul(u64, size.width, size.height) catch 2048;
+        const area = mul(u64, display.*.width, display.*.height) catch 4096;
         var snake = Snake{
             .x = try alloc.alloc(u64, area),
             .y = try alloc.alloc(u64, area),
@@ -69,8 +124,8 @@ const Snake = struct {
             y.* = 0;
         }
 
-        snake.y[0] = size.height >> 1;
-        snake.x[0] = size.width >> 1;
+        snake.y[0] = display.*.height;
+        snake.x[0] = display.*.width >> 1;
         
         return snake;
     }
@@ -81,8 +136,6 @@ const Snake = struct {
     }
 
     pub fn advance(self: *Snake) !void {
-        
-        const size = try zbox.size();
         
         var snakeHead = Coord {
             .x = self.*.x[self.*._head],
@@ -96,16 +149,14 @@ const Snake = struct {
 
         if (self.*.dir) |s| {
 
-            self.*.display.cellRef(snakeHead.y, snakeHead.x).* = self.*.bodyCell;
             if (!self.*.dead) {
                 switch (s) {
                     .left => if (snakeHead.x > 0) {snakeHead.x -= 1;},
-                    .right => if (snakeHead.x < size.width - 1) {snakeHead.x += 1;},
+                    .right => if (snakeHead.x < self.display.*.width - 1) {snakeHead.x += 1;},
                     .up => if (snakeHead.y > 0) {snakeHead.y -= 1;},
-                    .down => if (snakeHead.y < size.height - 1) {snakeHead.y += 1;},
+                    .down => if (snakeHead.y < (self.display.*.height << 1) - 1) {snakeHead.y += 1;},
                 }
 
-                if (self.*.display.cellRef(snakeHead.y, snakeHead.x).*.attribs.bg_green == true) self.*.dead = true;
 
                 self.*.x[self.*._tail] = snakeHead.x;
                 self.*.y[self.*._tail] = snakeHead.y;
@@ -114,9 +165,11 @@ const Snake = struct {
 
                 self.*._tail = sub(u64, self.*._tail, 1) catch sub(u64, self.*.length, 1) catch unreachable;
 
-                self.*.display.cellRef(snakeHead.y, snakeHead.x).* = if (self.*.dead) self.*.deadCell else self.*.headCell;
+                try self.*.display.setCell(snakeTail.x, snakeTail.y, false);
+                if (self.display.getCell(snakeHead.x, snakeHead.y)) self.*.dead = true;
                 
-                self.*.display.cellRef(snakeTail.y, snakeTail.x).* = .{.char = ' ',};
+                try self.*.display.setCell(snakeHead.x, snakeHead.y, true);
+
             }
         }
     }
@@ -135,7 +188,9 @@ pub fn main() !void {
     var output = try zbox.Buffer.init(allocator, size.height, size.width);
     defer output.deinit();
 
-    var snake = try Snake.init(allocator, &output, 60);
+    var grid = Grid.init(&output, 0, 1, size.width, size.height - 1);
+
+    var snake = try Snake.init(allocator, &grid, 30);
     defer snake.deinit();
 
     while (try zbox.nextEvent()) |e| {
